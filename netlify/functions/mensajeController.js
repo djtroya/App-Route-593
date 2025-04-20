23b59ceac1f862199ab978b57ca8f639eabb33b4
Erick Parada <37272682+djtroya@users.noreply.github.com>
2025-04-20 15:13:18 -0500

    Update mensajeController.js

--- netlify/functions/mensajeController.js
+++ netlify/functions/mensajeController.js
@@ -1,8 +1,15 @@
 const supabase = require('@supabase/supabase-js');
 const { createClient } = supabase;
 
+// Obtener las variables de entorno
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_KEY;
+
+// Agregar logs para verificar si las variables están siendo cargadas correctamente
+console.log('Supabase URL:', supabaseUrl);
+console.log('Supabase Key:', supabaseKey);
+
+// Crear el cliente de Supabase
 const client = createClient(supabaseUrl, supabaseKey);
 
 async function procesarMensaje({ phone, location, urbanization, destination }) {
