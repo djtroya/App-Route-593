f2629e933667f4e6da234cd02a72617f177dbfed
Erick Parada <37272682+djtroya@users.noreply.github.com>
2025-04-29 18:22:46 -0500

    Update clientes.js

--- netlify/functions/routes/clientes.js
+++ netlify/functions/routes/clientes.js
@@ -1,1 +1,14 @@
+const express = require('express');
+const router = express.Router();
+const {
+  listarClientes,
+  editarCliente,
+} = require('../controllers/clientesController');
 
+// Ruta para obtener todos los clientes
+router.get('/', listarClientes);
+
+// Ruta para editar un cliente por ID
+router.put('/:id', editarCliente);
+
+module.exports = router;
