5f0f9244caa43f91b591a336556b2da3249d803b
Erick Parada <37272682+djtroya@users.noreply.github.com>
2025-04-30 13:17:16 -0500

    Create panelClientes.js

---
+++ netlify/functions/panelClientes.js
@@ -0,0 +1,6 @@
+// /netlify/functions/routes/clientes.js
+const { handler } = require('../controllers/clientesController');
+
+exports.handler = async (event, context) => {
+  return handler(event, context);
+};
